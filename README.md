b57e1888af4a1375658a71a58926e2ae621f1ec6
Glitch <76911308+JBYT27@users.noreply.github.com>
2021-06-10 13:41:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Blob!
+> Your general interesting discord bot!
+
+## About
+Blob! is your general discord bot, and it has things like:
+- A naughty word patrol (lol)
+- Fun commands
+- Memes
---
+++ main.py
@@ -0,0 +1,57 @@
+import discord
+import os
+import json
+import requests
+import random
+
+client = discord.Client()
+
+nono_words = ["nincompoop", "booger", "depressed", "blob sucks", "boogers"]
+great_words = ["i am awesome", ""]
+
+nono_msg = [
+  "Don't say nono words! :<",
+  "Don't do that again!",
+  "You are not pog :<"
+]
+yes_msg = [
+  "Great! You are now super pog!",
+  "You are now super famous!",
+  "Awww, no you don't! You're awesome!"
+]
+
+PREFIX = "-"
+
+def thync():
+  res = requests.get("https://thync-API.jbloves27.repl.co/toggle")
+  data = res.content
+  return data
+
+@client.event
+async def on_ready():
+  print(f"Bot logged in as {client.user}")
+
+@client.event
+async def on_message(msg):
+  if msg.author == client.user:
+    return
+
+  MSG = msg.content
+
+  if MSG.startswith(f"{PREFIX}help"):
+    await msg.channel.send(f"""
+    ```Commands:
+    Syntax: {PREFIX}command [optional text]
+    Prefix: `{PREFIX}`
+
+    `{PREFIX}help [optional text]`: Helps with bot commands.
+    `{PREFIX}search [text]`: Searches google.
+    `{PREFIX}thync [on/off]`: Turns on/off thync.
+    `{PREFIX}memes [optional text]`: Memes.
+    ```""")
+  
+  if any(word in MSG for word in nono_words):
+    await msg.channel.send(random.choice(nono_msg))
+
+
+client.run(os.environ["TOKEN"])
